11e0e5b403ee32f6b4b2ef4aa2b9aef31bfdb632
Nirupam-paul <58510626+Nirupam-paul@users.noreply.github.com>
2021-02-08 11:30:27 +0530

    register fix

--- register.php
+++ register.php
@@ -1,44 +1,48 @@
-<!DOCTYPE html>
-<html lang="en">
+<?php 
+include('header.php'); 
+?>
 
-<head>
-    <meta charset="utf-8">
-    <title>Register</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
-</head>
 
-<body>
 
-    <div container>
-        <div class="row">
-            <div class="col-6 offset-3">
-                <div class="card " style="margin-top: 25px;">
-                    <div class="card-header text-center">
-                        <h1 style="color: red;">Register Here </h1>
-                    </div>
-                    <div class="card-body">
-                        <form method="POST" class="row g-3" >
-                            <div class="col-md-6">
-                                <label for="inputEmail4" class="form-label">Name</label>
-                                <input type="text" class="form-control" id="inputEmail4" name="name">
+<section id="hero-4" class="bg-fixed hero-section division">
+	<div class="container">
+        <h3 class="h3-sm steelblue-color text-center">Register Here</h3>
+        <div id="hero-section-form" class="text-center mb-40">
+            <form method="POST" class="row g-3">
+                            <div class="col-md-6 mt-3">
+                                <input type="text" style="height:50px;" class="form-control" id="inputEmail4" name="name" placeholder="Your Name*">
                             </div>
-                            <div class="col-md-6">
-                                <label for="inputPassword4" class="form-label">Age</label>
-                                <input type="number" class="form-control" id="inputPassword4" name="age">
+                            <div class="col-md-6 mt-3">
+                                <input type="number" style="height:50px;" class="form-control" id="inputPassword4" name="age" placeholder="Your Age*">
                             </div>
-                            <div class="col-md-6">
-                                <label for="inputState" class="form-label">Gender</label>
-                                <select id="inputState" class="form-select" name="gender">
-                                    <option selected>Male</option>
+                            <div class="col-md-6 mt-3">
+                                <input type="email" style="height:50px;" class="form-control" id="inputEmail4" name="email" placeholder="Your Email*">
+                            </div>
+                            <div class="col-md-6 mt-3">
+                                <input type="number" style="height:50px;" class="form-control" id="inputPassword4" name="phone" placeholder="Your Phonenumber*">
+                            </div>
+                            <div class="col-md-6 mt-3"> 
+                                <select id="inputState" style="height:50px; 
+                                    width:100%; border:1px solid #ced4da;
+                                    transition: border-color .15s ease-in-out,box-shadow .15s ease-in-out;
+                                    color: #495057;
+                                    border-radius:.25rem;"
+                                    class="form-select" name="gender">
+                                    <option >Gender*</option>
+                                    <option >Male</option>
                                     <option>Female</option>
                                     <option>Others</option>
                                 </select>
                             </div>
-                            <div class="col-md-6">
-                                <label for="inputState" class="form-label">BloodGroup</label>
-                                <select id="inputState" class="form-select" name="blood">
-                                    <option selected>A+</option>
+                            <div class="col-md-6 mt-3">
+                                <select id="inputState" style="height:50px; 
+                                    width:100%; border:1px solid #ced4da;
+                                    transition: border-color .15s ease-in-out,box-shadow .15s ease-in-out;
+                                    color: #495057;
+                                    border-radius:.25rem;" 
+                                    class="form-select" name="blood">
+                                    <option >BloodGroup*</option>
+                                    <option >A+</option>
                                     <option>A-</option>
                                     <option>B+</option>
                                     <option>B-</option>
@@ -48,49 +52,21 @@
                                     <option>O-</option>
                                 </select>
                             </div>
-                            <div class="col-md-6">
-                                <label for="inputEmail4" class="form-label">Email</label>
-                                <input type="email" class="form-control" id="inputEmail4" name="email">
+                            <div class="col-6 mt-3">
+                                <input type="password" style="height:50px;" class="form-control" id="inputAddress" name="password" placeholder="Password">
                             </div>
-                            <div class="col-md-6">
-                                <label for="inputPassword4" class="form-label">Phone</label>
-                                <input type="number" class="form-control" id="inputPassword4" name="phone">
+                            <div class="col-6 mt-3">
+                                <input type="password" style="height:50px;" class="form-control" id="inputAddress" name="confirm_password" placeholder="Password">
                             </div>
-                            <div class="col-12">
-                                <label for="inputAddress" class="form-label">Password</label>
-                                <input type="password" class="form-control" id="inputAddress" name="password">
+                            <div class="col-12 text-center mt-5 mb-5">
+                                <button type="submit" class="btn btn-orange btn-md btn-tra-black blue-hover" style="width: 50%;" name="employee_register_submit">Register</button>
                             </div>
-                            <div class="col-12">
-                                <label for="inputAddress2" class="form-label">Confirm password</label>
-                                <input type="password" class="form-control" id="inputAddress2" name="confirm_password">
-                            </div>
-
-                            <div class="col-12" style="text-align: left;">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" id="gridCheck">
-                                    <label class="form-check-label" for="gridCheck">
-                                        I accept all the Terms and Condition
-                                    </label>
-                                </div>
-                            </div>
-                            <div class="col-12 text-center">
-                                <button type="submit" class="btn btn-danger" style="width: 50%;" name="register_submit">Register</button>
-                            </div>
-                        </form>
-                    </div>
-                    <div class="card-footer text-center ">
-                        <p>Already a memeber ! <a href="login.php" style="color: red;">Login</a></p>
-                    </div>
-                </div>
-            </div>
-        </div>
+                        </form> 
+		</div>
     </div>
-
-
+</section>
 
     <?php
-    include("databaseconnect.php");
-    include('function.php');
     if (isset($_POST["register_submit"])) {
         $name = $_POST["name"];
         $age = $_POST["age"];
@@ -124,7 +100,5 @@
             echo "<script>alert('Password didnot match')</script>";
         }
     }
+    include('footer.php');
     ?>
-</body>
-
-</html>
